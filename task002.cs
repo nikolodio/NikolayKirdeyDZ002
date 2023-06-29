816900b5f6343c781dd6fa39df5c7202e3e8d9e6
nikolodio <136374878+nikolodio@users.noreply.github.com>
2023-06-29 14:39:35 +0300

    Add files via upload
    task002 second version, check pls

---
+++ task002.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Enter the number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+string stringNumber = Convert.ToString(number);
+if (stringNumber.Length > 2)
+{
+    Console.WriteLine("three digit -> " + stringNumber[2]);
+}
+else
+{
+    Console.WriteLine("-> no third digit");
+}
---
+++ task003.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Enter the day of the week");
+int dayNumber = Convert.ToInt32(Console.ReadLine());
+int[] array = { 1, 2, 3, 4, 5, 6, 7 };
+if (dayNumber <= 5)
+{
+    Console.WriteLine($"{dayNumber} -> no, this day is nit holiday");
+}
+else
+{
+    Console.WriteLine($"{dayNumber} -> yes, this day is holiday ");
+}
