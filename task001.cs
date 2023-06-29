854fe892606201951b1c8adf0b8c566612cf96ea
Nikolas White <nikolodio.k@gmail.com>
2023-06-29 12:54:41 +0300

    first version

---
+++ task001.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine("Enter a three-digit number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+string stringNumber = Convert.ToString(number);
+Console.WriteLine("Secnod digit this number -> "+stringNumber[1]);
